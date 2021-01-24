d55be9cebfbaa2728cf04038ff103e4a8a16dfd2
memduh kocadağ <93190000495@ogrenci.ege.edu.tr>
2021-01-24 12:56:00 +0300

    asda

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] tKeys;
+        TValue[] tValues;
+        public MyDictionary() 
+        {
+            tKeys = new TKey[0];
+            tValues = new TValue[0];
+        }
+        public void Add(TKey key, TValue value) 
+        {
+            TKey[] _tKeys = tKeys;
+            tKeys = new TKey[tKeys.Length + 1];
+            for (int i = 0; i < _tKeys.Length; i++)
+            {
+                tKeys[i] = _tKeys[i];
+            }
+            tKeys[tKeys.Length - 1] = key;
+            TValue[] _tValues = tValues;
+            tValues = new TValue[tValues.Length + 1];
+            for (int i = 0; i < _tValues.Length; i++)
+            {
+                tValues[i] = _tValues[i];
+            }
+            tValues[tValues.Length - 1] = value;
+        }
+        public int Counter 
+        {
+            get 
+            {
+                return tKeys.Length;
+            }
+        }
+        public void Iterate() 
+        {
+            for (int i = 0; i < tKeys.Length; i++)
+            {
+                Console.WriteLine(tKeys[i]+ ".eleman");
+                Console.WriteLine(tValues[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Another");
+            myDictionary.Add(2, "Human Being");
+
+            Console.WriteLine(myDictionary.Counter-1);
+            myDictionary.Iterate();
+        }
+    }
+}
